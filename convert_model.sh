47b4d73277477d5f095cba0dc1b1936a721aaf8e
eseshinpu <ko4666@gmail.com>
2024-01-27 12:11:08 +0000

    fix/quick fix.

--- convert_model.sh
+++ convert_model.sh
@@ -11,7 +11,7 @@ mkdir -p $OUT_DIR
 
 function convert_onnx2trt () {
     OUT_PATH=${OUT_DIR}/$(basename $1 .onnx).engine
-    trtexec --onnx=$1 --minShapes=input:1x3x8x8 --optShapes=input:1x3x64x64 --maxShapes=input:1x3x1080x1920 --saveEngine=$OUT_PATH --tacticSources=+CUDNN,-CUBLAS,-CUBLAS_LT --int8 --workspace=4096
+    trtexec --onnx=$1 --minShapes=input:1x3x8x8 --optShapes=input:1x3x64x64 --maxShapes=input:1x3x1080x1920 --saveEngine=$OUT_PATH --tacticSources=+CUDNN,-CUBLAS,-CUBLAS_LT --fp16 --workspace=4096
 }
 
 convert_onnx2trt $1
