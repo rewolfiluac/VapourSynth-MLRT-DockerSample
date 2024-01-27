d88d4946292b57be0995766be7a4698fab94e072
eseshinpu <ko4666@gmail.com>
2024-01-27 11:49:44 +0000

    [fix] quick fix.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # VideoToolKit
+This repository is a Docker and code sample for super-resolution using VapourSynth and vs-mlrt.<br>
+このリポジトリは、VapourSynthとvs-mlrtを使った超解像を行うためのDocker&コードサンプルです。<br>
+<br>
+In this environment, TensorRT is used, but please build other libraries (vsov for OpenVINO, vsncnn for NCNN) as needed.<br>
+この環境ではTensorRTを利用してますが、必要に応じて他のライブラリ(vsov:OpenVINO用やvsncnn:NCNN用)もビルドしてください。<br>
+
 
 # Setup
 ```bash
@@ -6,4 +12,12 @@
 echo "JOBS=$(($(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g') + 1))" > .env
 # docker build
 docker compose up -d --build
+# join container
+docker compose exec develop /bin/bash
+# model convert
+bash convert_model.sh /tmp/models/cugan/pro-conservative-up2x.onnx
+# upconv & encode demo
+# before executing, please place the video in the input directory.
+cd src
+python demo_trt.py
 ```
--- convert_model.sh
+++ convert_model.sh
@@ -1,25 +1,17 @@
 #! /bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <model_dir>"
+    echo "Usage: $0 <onnx_model_path>"
     exit 1
 fi
 
-MODEL_DIR=$1
 OUT_DIR=/trtengine
 
 mkdir -p $OUT_DIR
 
 function convert_onnx2trt () {
     OUT_PATH=${OUT_DIR}/$(basename $1 .onnx).engine
-    trtexec --onnx=$1 --minShapes=input:1x3x8x8 --optShapes=input:1x3x64x64 --maxShapes=input:1x3x1080x1920 --saveEngine=$OUT_PATH --tacticSources=+CUDNN,-CUBLAS,-CUBLAS_LT --fp16 --workspace=4096
+    trtexec --onnx=$1 --minShapes=input:1x3x8x8 --optShapes=input:1x3x64x64 --maxShapes=input:1x3x1080x1920 --saveEngine=$OUT_PATH --tacticSources=+CUDNN,-CUBLAS,-CUBLAS_LT --int8 --workspace=4096
 }
 
-function convert_onnx2trt_all () {
-    for filepath in $1/*; do
-        echo $filepath
-        convert_onnx2trt $filepath
-    done
-}
-
-convert_onnx2trt_all ${MODEL_DIR}/cugan
+convert_onnx2trt $1
